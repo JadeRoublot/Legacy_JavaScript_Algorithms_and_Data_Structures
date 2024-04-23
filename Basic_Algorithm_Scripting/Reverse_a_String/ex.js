1f2d4a552039aac0adc11c79bb83a800b62c0ba5
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2024-04-23 15:10:11 +0200

    Update

---
+++ Basic_Algorithm_Scripting/Reverse_a_String/ex.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+
+    let result = "";
+  
+    for(let i = str.length - 1; i >= 0; i--){
+  
+      result += str[i];
+    }
+    return result;
+  }
+  
+  reverseString("hello");
+  reverseString("Howdy");
+  reverseString("Greetings from Earth");
